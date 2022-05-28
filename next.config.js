3bcbadfd9747c316e683a4317828a95b3321b5c9
yuiseki <yuiseki@gmail.com>
2022-05-28 18:46:17 +0900

    add basePath to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/scrapbox-nextjs-template",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
