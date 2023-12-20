31ae574c808ad5c722c93fa276fe88690177d7d0
yuiseki <yuiseki@users.noreply.github.com>
2023-12-20 00:06:48 +0000

    deploy: 882d05b57fc0fcf5f4f7aa95d436d2d373622ea7

---
+++ _next/static/MWlAOtRpynzOiuaBGmFJS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b5667403f1d8ddd3.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],"/[title]":["static/chunks/pages/[title]-569dcb5b5f81c9ac.js"],sortedPages:["/","/_app","/_error","/[title]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
