5d8bc3595621c921806ee759488b6c812d72a067
yuiseki <yuiseki@users.noreply.github.com>
2024-10-28 00:06:02 +0000

    deploy: 6a472bf2c5a30431bbfa43a871f9e71db96b6c62

--- _next/static/Zf3Imcg-Kc9uAprtFdHBI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2240c90ca17a0dd7.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/[title]":["static/chunks/pages/[title]-b96b98c25ebb860f.js"],sortedPages:["/","/_app","/_error","/[title]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
